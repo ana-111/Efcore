87d7657824d018762fa448e3f064673e253ae495
ana-111 <anacarolinaamaralsilva2003@gmail.com>
2020-09-09 01:14:05 -0300

    Efcore finalizado

---
+++ Contexts/PedidoContext.cs
@@ -0,0 +1,25 @@
+﻿using Efcore.Domains;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Efcore.Contexts
+{
+    public class PedidoContext : DbContext
+    {
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<PedidoItem> PedidosItens { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(@"Data Source=AMARALSILVANOVO\SQLEXPRESS;Persist Security Info=True;User ID=sa;Password=sa132");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
